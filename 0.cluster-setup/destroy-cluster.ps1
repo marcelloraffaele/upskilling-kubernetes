9e9516d1997d48ac17c3d9dd1ec4377bfce34e13
Raffaele Marcello <rmarcello@microsoft.com>
2025-05-28 11:44:53 +0200

    Add command to stop AKS cluster before deletion

--- 0.cluster-setup/destroy-cluster.ps1
+++ 0.cluster-setup/destroy-cluster.ps1
@@ -1,8 +1,10 @@
 $AKS_RESOURCE_GROUP="upskilling-k8s-rg"
 $LOCATION="italynorth"
 
+az aks stop --name $AKS_NAME `
+    --resource-group $AKS_RESOURCE_GROUP
+
 az group delete --location $LOCATION `
     --name $AKS_RESOURCE_GROUP `
     --yes `
     --no-wait
-    
