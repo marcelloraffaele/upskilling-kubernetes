46579c7a87a05a220b575d6191533f55bf10ac0b
Raffaele Marcello <rmarcello@microsoft.com>
2025-09-29 23:05:39 +0200

    Remove unused LOCATION variable from destroy-cluster script

--- 00.cluster-setup/99-destroy-cluster.ps1
+++ 00.cluster-setup/99-destroy-cluster.ps1
@@ -1,10 +1,8 @@
 $AKS_RESOURCE_GROUP="upskilling-k8s-rg"
-$LOCATION="italynorth"
 
 az aks stop --name $AKS_NAME `
     --resource-group $AKS_RESOURCE_GROUP
 
-az group delete --location $LOCATION `
-    --name $AKS_RESOURCE_GROUP `
+az group delete --name $AKS_RESOURCE_GROUP `
     --yes `
     --no-wait
