d59e0f36eb84d7b59cd4f05e6b7362b8e54ff73c
Raffaele Marcello <rmarcello@microsoft.com>
2025-11-26 23:05:13 +0100

    Add Azure Key Vault integration scripts and sample pod definition; create Key Vault, assign roles, and define SecretProviderClass

--- 07-Module 5/1.keyvault-integration/1.prep.ps1
+++ 07-Module 5/1.keyvault-integration/1.prep.ps1
@@ -15,6 +15,19 @@ $KEYVAULT_NAME="upskillingkv$((Get-Random -Maximum 10000))"
 az keyvault create --name $KEYVAULT_NAME --resource-group $AKS_RESOURCE_GROUP --location $LOCATION --enable-rbac-authorization
 
 ## Update an existing Azure key vault
-az keyvault update --name $KEYVAULT_NAME --resource-group $AKS_RESOURCE_GROUP --location $LOCATION --enable-rbac-authorization
+#az keyvault update --name $KEYVAULT_NAME --resource-group $AKS_RESOURCE_GROUP --location $LOCATION --enable-rbac-authorization
 
+
+#####
+# Get your user's object ID
+$USER_OBJECT_ID = (az ad signed-in-user show --query id -o tsv)
+
+# Get the Key Vault resource ID
+$KEYVAULT_ID = (az keyvault show --name $KEYVAULT_NAME --resource-group $AKS_RESOURCE_GROUP --query id -o tsv)
+
+# Assign Key Vault Secrets Officer role
+az role assignment create --role "Key Vault Secrets Officer" --assignee $USER_OBJECT_ID --scope $KEYVAULT_ID
+####
+
+#set a plain-text secret called ExampleSecret
 az keyvault secret set --vault-name $KEYVAULT_NAME --name ExampleSecret --value MyAKSExampleSecret
--- 07-Module 5/1.keyvault-integration/2.access-the-kv-using-secret-store.ps1
+++ 07-Module 5/1.keyvault-integration/2.access-the-kv-using-secret-store.ps1
@@ -1,3 +1,33 @@
 # https://learn.microsoft.com/en-us/azure/aks/csi-secrets-store-identity-access?tabs=azure-portal&pivots=access-with-a-user-assigned-managed-identity
+$AKS_RESOURCE_GROUP="upskilling-k8s-rg"
+$AKS_NAME="upskilling-aks"
+$CONNECTION_NAME="myKVConnection"
+# Be sure this is the name of the key vault you created in the previous exercise
+$KEYVAULT_NAME="upskillingkv3455"
 
-# TBC
+
+#####################################
+$IDENTITY_OBJECT_ID= az aks show --resource-group $AKS_RESOURCE_GROUP --name $AKS_NAME --query addonProfiles.azureKeyvaultSecretsProvider.identity.objectId -o tsv
+$IDENTITY_CLIENT_ID=az aks show --resource-group $AKS_RESOURCE_GROUP --name $AKS_NAME --query addonProfiles.azureKeyvaultSecretsProvider.identity.clientId -o tsv
+
+az role assignment create --role "Key Vault Certificate User" --assignee $IDENTITY_OBJECT_ID --scope $KEYVAULT_ID
+
+$IDENTITY_OBJECT_ID
+# 9bd98cf4-c7f2-445d-a841-7c0126aacac0
+$IDENTITY_CLIENT_ID
+# 7904a528-77ee-4eb9-a9f6-a34ef3f35c92
+
+
+$NS="kvsecretsstore-demo"
+kubectl create namespace $NS
+kubectl create -f 3.secret_provider_class.yaml -n $NS
+kubectl create -f 4.pod.yaml -n $NS
+
+kubectl exec busybox-secrets-store-inline-user-msi -n $NS -- ls /mnt/secrets-store/
+
+kubectl exec busybox-secrets-store-inline-user-msi -n $NS -- cat /mnt/secrets-store/ExampleSecret
+
+# Clean up
+kubectl delete -f 4.pod.yaml -n $NS
+kubectl delete -f 3.secret_provider_class.yaml -n $NS
+kubectl delete namespace $NS
